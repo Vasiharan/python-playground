df9ab7eb5b0b3214f80682ac81b39d042bf92cdb
Vasi Logeswaran <Vasi.Logeswaran@gmail.com>
2018-07-02 16:15:05 +0100

    Simple data type code

---
+++ dataTypes.py
@@ -0,0 +1,32 @@
+# Integer
+result = 0
+for i in range(10):
+    result += i
+print(result)
+
+# List
+L = list(range(10))
+print(L)
+
+typeL = type(L[0])
+print(typeL)
+
+# List of strings
+L2 = [str(c) for c in L]
+print(L2)
+
+L3 = [True, "2", 3.0, 4]
+L4 = [type(item) for item in L3]
+print(L4)
+
+
+
+
+
+
+
+
+
+
+
+
